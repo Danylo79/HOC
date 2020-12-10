367dcaa641a3169113e1d7bcd9cc7543102200bb
BuildTools <dan.komisarenko@gmail.com>
2020-12-10 12:13:25 -0500

    Fixed directory naming (.gitignore confusion)

---
+++ src/main/java/dev/dankom/hoc/commands/Command.java
@@ -0,0 +1,5 @@
+package dev.dankom.hoc.commands;
+
+public class Command {
+
+}
--- src/main/java/dev/dankom/hoc/file/FileManager.java
+++ src/main/java/dev/dankom/hoc/file/FileManager.java
@@ -4,6 +4,6 @@ import dev.dankom.hoc.file.json.JsonFile;
 import dev.dankom.hoc.file.json.JsonObjectBuilder;
 
 public class FileManager {
-    public Directory root = new Directory("./HOC");
+    public Directory root = new Directory("./Hour_Of_Code");
     public JsonFile privateFile = new JsonFile(root, "private", new JsonObjectBuilder().addKeyValuePair("token", "").build());
 }
